33e45ce1ebe2bafb838521028153effe83bc08ea
Mcdave <mcdavegbenga@gmail.com>
2022-08-18 11:12:56 +0100

    Updates

---
+++ precache-manifest.7981dfefc2b3a28f7e343da52d2f2d05.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "1f25797f451b4afb5794",
+    "url": "/GPT3_MCDAVE/static/css/main.5e9f5e5d.chunk.css"
+  },
+  {
+    "revision": "1f25797f451b4afb5794",
+    "url": "/GPT3_MCDAVE/static/js/main.1f25797f.chunk.js"
+  },
+  {
+    "revision": "b80769ea712c754fd21e",
+    "url": "/GPT3_MCDAVE/static/js/1.b80769ea.chunk.js"
+  },
+  {
+    "revision": "8c77ba111eaf536d2a04",
+    "url": "/GPT3_MCDAVE/static/js/runtime~main.8c77ba11.js"
+  },
+  {
+    "revision": "a95f42a40ce776c07a2891c220f620ae",
+    "url": "/GPT3_MCDAVE/static/media/logo.a95f42a4.svg"
+  },
+  {
+    "revision": "fea810ed22f6fc37fab045129a01ed07",
+    "url": "/GPT3_MCDAVE/static/media/blog01.fea810ed.png"
+  },
+  {
+    "revision": "e76b2c16b1e4ccf994a24eda75591951",
+    "url": "/GPT3_MCDAVE/static/media/blog02.e76b2c16.png"
+  },
+  {
+    "revision": "404d1a405e0cb0936f224fcd374c6cb0",
+    "url": "/GPT3_MCDAVE/static/media/blog03.404d1a40.png"
+  },
+  {
+    "revision": "d682b195d844afb2a4ce1e8573602221",
+    "url": "/GPT3_MCDAVE/static/media/blog04.d682b195.png"
+  },
+  {
+    "revision": "576efe185f7f571f39d782fde1d28fa9",
+    "url": "/GPT3_MCDAVE/static/media/blog05.576efe18.png"
+  },
+  {
+    "revision": "3d84899326ac5f0f66149fa037693d18",
+    "url": "/GPT3_MCDAVE/static/media/people.3d848993.png"
+  },
+  {
+    "revision": "17ade55739aedbbb39e78442cdad6fb3",
+    "url": "/GPT3_MCDAVE/static/media/ai.17ade557.png"
+  },
+  {
+    "revision": "3c84ae2642b58c3bc28340a7e763fc23",
+    "url": "/GPT3_MCDAVE/static/media/possibility.3c84ae26.png"
+  },
+  {
+    "revision": "6a321d0635745ca73cff91caff9fd2d3",
+    "url": "/GPT3_MCDAVE/index.html"
+  }
+];
